a528aba6dea1f6fa4e71cafba72666d20a55d821
Mia Patrikios <mia.patrikios@ixsystems.com>
2025-11-05 16:43:20 -0500

    create single file for prototype demonstration

---
+++ backend/prototype_demonstration.py
@@ -0,0 +1,17 @@
+# input resume and Clifton Strengths from candidates from local storage into AI Context Understanding
+# input job description and desired Clifton Strengths from the recruiter into AI Context Understanding
+# for demonstration, we should just show one example of each and print out the results at each step
+# this way we aren't waiting for external system to process 30+ documents
+
+# this means we need to have parsed resumes and Clifton Strengths for at least 15-20 candidates in DB prior to demonstration
+
+
+
+# Run similarity search 
+# Print all candidates that match based on skills and experience
+
+# Run ranking algorithm on Clifton strengths
+# Print candidates that matched based on Clifton strengths
+
+
+# Demonstrate final ranked list corresponding to job description and desired Clifton strengths
